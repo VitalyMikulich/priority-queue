2127c860a6ecf07f3f645c520335b0dc36c49322
VitalyMikulich <mr.wital1999@gmail.com>
2019-03-24 23:14:41 +0300

    solution of task

--- src/node.js
+++ src/node.js
@@ -1,23 +1,81 @@
 class Node {
-	constructor(data, priority) {
+    constructor(data, priority) {
+        this.data = data,
+        this.priority = priority,
+        this.parent = null,
+        this.left = null,
+        this.right = null
+    }
 
-	}
+    appendChild(node) {
+        if (!this.right) {
+			this.left ? this.right = node : this.left = node;
+		}
+        node.parent = this;
+    }
 
-	appendChild(node) {
+    removeChild(node) {
+        if (node != this.left && node != this.right) {
+			throw "Error";
+		}
+        if (node == this.left) {
+			this.left = null;
+		}
+        if (node == this.right) {
+			this.right = null;
+		}
+        node.parent = null;
+    }
 
-	}
+    remove() {
+        if (this.parent) {
+			this.parent.removeChild(this);
+		}
+    }
 
-	removeChild(node) {
+    swapWithParent() {
+        if (this.parent) {
+            let tmp = {}
+			if (this.parent.left &&  this.parent.left != this) {
+				this.parent.left.parent = this;
+			}
+            if (this.left)	{
+				this.left.parent = this.parent;
+			}
+            if (this.parent.right && this.parent.right != this)	{
+				this.parent.right.parent = this;
+			}
+            if (this.right)	{
+				this.right.parent = this.parent;
+			}
 
-	}
+            if (this.parent.parent) {
+                if (this.parent == this.parent.parent.left) {
+                    this.parent.parent.left = this;
+                }
+                if (this.parent == this.parent.parent.right) {
+                    this.parent.parent.right = this;
+                }
+			}
+			
+            tmp.right = this.right;
+            this.right = this.parent.right;
+            this.parent.right = tmp.right;
+            tmp.left = this.left;
+            this.left = this.parent.left;
+            this.parent.left = tmp.left;
+            tmp.parent = this.parent;
+            this.parent = this.parent.parent;
+            tmp.parent.parent = this;
 
-	remove() {
-
-	}
-
-	swapWithParent() {
-		
-	}
+            if (this.left == this) {
+				this.left = tmp.parent;
+			}
+            if (this.right == this) {
+				this.right = tmp.parent;
+			}
+        }
+    }
 }
 
 module.exports = Node;
